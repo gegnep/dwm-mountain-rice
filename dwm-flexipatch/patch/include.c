2c6e8bc92de9b62abf8e06bbe195f8f9e6ea4759
gegnep <96670443+gegnep@users.noreply.github.com>
2022-02-04 19:53:29 -0600

    Add files via upload

---
+++ dwm-flexipatch/patch/include.c
@@ -0,0 +1,23 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_alpha.c"
+#include "combo.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "cyclelayouts.c"
+#include "pertag.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_centeredmaster.c"
+#include "layout_fibonacci.c"
+#include "layout_grid.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
